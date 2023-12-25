7607540963e84d2a2170f059b774822f94de22c4
miyajimakatsumi <48985869+katsulau@users.noreply.github.com>
2023-12-25 16:09:07 +0100

    add starter webflux

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:3.2.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
